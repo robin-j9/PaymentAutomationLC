d0eaf0f7baac027eb3924a2b6f89708e3732f1f2
robin-j9 <robin@robinjun.com>
2020-05-21 03:17:02 -0500

    added ability to edit payment profile

--- PaymentAutomationLC/Controllers/PaymentController.cs
+++ PaymentAutomationLC/Controllers/PaymentController.cs
@@ -108,5 +108,39 @@ namespace PaymentAutomationLC.Controllers
                 return View(paymentProfileViewModel);
             }
         }
+
+        public IActionResult EditProfile(int id)
+        {
+            PaymentProfile profileToEdit = context.PaymentProfiles.Single(p => p.ID == id);
+            PaymentProfileViewModel paymentProfileViewModel = new PaymentProfileViewModel
+            {
+                Name = profileToEdit.Name,
+                PayPerArticle = profileToEdit.PayPerArticle,
+                ArticleBonus = profileToEdit.ArticleBonus,
+                MinimumPVForBonus = profileToEdit.MinimumPVForBonus,
+                PaymentProfileID = id
+            };
+            return View(paymentProfileViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult EditProfile(PaymentProfileViewModel paymentProfileViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                PaymentProfile profileToEdit = context.PaymentProfiles.Single(p => p.ID == paymentProfileViewModel.PaymentProfileID);
+                profileToEdit.Name = paymentProfileViewModel.Name;
+                profileToEdit.PayPerArticle = paymentProfileViewModel.PayPerArticle;
+                profileToEdit.ArticleBonus = paymentProfileViewModel.ArticleBonus;
+                profileToEdit.MinimumPVForBonus = paymentProfileViewModel.MinimumPVForBonus;
+
+                context.SaveChanges();
+                return Redirect("/Payment/Profiles");
+            } 
+            else
+            {
+                return View(paymentProfileViewModel);
+            }
+        }
     }
 }
