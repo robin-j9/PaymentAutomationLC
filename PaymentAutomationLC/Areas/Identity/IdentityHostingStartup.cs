56f990f92c78ac3bd677a0d8714d87cc4ed6e9ec
robin-j9 <robin@robinjun.com>
2020-05-24 19:27:44 -0500

    added properties to RegisterModel and updated Register form

---
+++ PaymentAutomationLC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PaymentAutomationLC.Data;
+using PaymentAutomationLC.Models;
+
+[assembly: HostingStartup(typeof(PaymentAutomationLC.Areas.Identity.IdentityHostingStartup))]
+namespace PaymentAutomationLC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
