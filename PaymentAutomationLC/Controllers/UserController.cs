8965aeb8687020b374d79864ea28e5b90e124275
robin-j9 <robin@robinjun.com>
2020-05-25 03:17:39 -0500

    added User list with info to User/Index

---
+++ PaymentAutomationLC/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using PaymentAutomationLC.Data;
+using PaymentAutomationLC.Models;
+using PaymentAutomationLC.ViewModels;
+
+namespace PaymentAutomationLC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly ApplicationDbContext context;
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext dbContext)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            context = dbContext;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            IList<string> roles;
+            List<IList<string>> userRoles = new List<IList<string>>();
+
+            foreach(var user in userManager.Users)
+            {
+                roles = await userManager.GetRolesAsync(user);
+                userRoles.Add(roles);
+            }
+
+            ViewUsersViewModel viewUsersViewModel = new ViewUsersViewModel
+            {
+                Users = userManager.Users,
+                Roles = userRoles
+            };
+
+            return View(viewUsersViewModel);
+        }
+    }
+}
--- PaymentAutomationLC/Startup.cs
+++ PaymentAutomationLC/Startup.cs
@@ -31,7 +31,7 @@ namespace PaymentAutomationLC
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
