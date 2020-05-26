c257364eefcc3132c8fb05bca5d238435efdd2ca
robin-j9 <robin@robinjun.com>
2020-05-26 01:31:19 -0500

    removed unused code from UserController

--- PaymentAutomationLC/Controllers/UserController.cs
+++ PaymentAutomationLC/Controllers/UserController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using PaymentAutomationLC.Data;
 using PaymentAutomationLC.Models;
 using PaymentAutomationLC.ViewModels;
 
@@ -14,14 +12,10 @@ namespace PaymentAutomationLC.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> userManager;
-        private readonly RoleManager<IdentityRole> roleManager;
-        private readonly ApplicationDbContext context;
 
-        public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext dbContext)
+        public UserController(UserManager<ApplicationUser> userManager)
         {
             this.userManager = userManager;
-            this.roleManager = roleManager;
-            context = dbContext;
         }
         public async Task<IActionResult> IndexAsync()
         {
